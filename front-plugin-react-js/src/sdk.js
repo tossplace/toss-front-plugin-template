1e465db4d12bd632a0fa4d95f9d456ca229839ca
joohaem-lee <joohaem.lee@toss.im>
2026-05-20 09:39:29 +0900

    fix: scaffold

--- front-plugin-react-js/src/sdk.js
+++ front-plugin-react-js/src/sdk.js
@@ -1,11 +1,11 @@
 const sdk = window.TossFrontSDK;
 
 sdk.overrides({
-  serialNumber: '000000000000000',
+  serialNumber: "000000000000000",
   merchant: {
     id: 0,
-    name: '테스트 매장',
-    businessNumber: '0000000000',
+    name: "테스트 매장",
+    businessNumber: "0000000000",
   },
 });
 
